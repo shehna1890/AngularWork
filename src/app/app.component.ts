1470345976ab4dbd9258e3da5cfe846702ab7a99
Shehna <Shehna@NTP-LAP-929.chn.nestgroup.net>
2022-11-24 13:24:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  public str="";
+  public a=3
+  public b=45
+  public c=this.a+this.b;
+  
+
+  data={
+    tech1: "Angular",
+    tech2:"Node"
+    
+    
+  }
+  myfunction(){
+    this.str="First Button";
+  }
+    myfunction1(){
+      this.str="Second Button";
+
+    }
+      public Employee=[
+        {"Name":"Shehna","Age":25},
+        {"Name":"Anjana","Age":23},
+        {"Name":"Fathima","Age":30},
+        {"Name":"Rohan","Age":89}
+   
+    ]
+     
+  
+   
   title = 'FirstDemo';
 }
